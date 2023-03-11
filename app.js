66de7fc4d91dc3891df40f5742d1298f4f5c92c6
cristian.passos <cristian.passos@sp.agence.com.br>
2023-03-10 22:27:29 -0400

    Upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const form = document.getElementById("search-form");
+const searchInput = document.getElementById("search-input");
+const searchResults = document.getElementById("search-results");
+
+form.addEventListener("submit", function(event) {
+	event.preventDefault();
+	const searchTerm = searchInput.value.trim().toLowerCase();
+	if (searchTerm === "bebida") {
+		searchResults.innerHTML = `
+			<article>
+				<h3><a href="#">Zé Delivery</a></h3>
+				<p>Entrega de bebidas em até 1h! Confira nossas promoções.</p>
+			</article>
+		`;
+	} else {
+		searchResults.innerHTML = `
+			<article>
+				<h3><a href="#">Título do Resultado da Pesquisa</a></h3>
+				<p>Descrição do resultado da pesquisa.</p>
+			</article>
+			<article>
+				<h3><a href="#">Título do Resultado da Pesquisa</a></h3>
+				<p>Descrição do resultado da pesquisa.</p>
+			</article>
+			<article>
+				<h3><a href="#">Título do Resultado da Pesquisa</a></h3>
+				<p>Descrição do resultado da pesquisa.</p>
+			</article>
+		`;
+	}
+});
